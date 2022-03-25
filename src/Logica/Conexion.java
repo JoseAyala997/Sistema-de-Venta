905db5030389ab89cf131d3fd446d7f503213c87
JoseAyala997 <ayalarolonjoseluis@gmail.com>
2022-03-24 22:40:15 -0300

    #7 cambios leves

--- src/Logica/Conexion.java
+++ src/Logica/Conexion.java
@@ -16,8 +16,8 @@ public class Conexion {
 
     public String db = "ventas";
     public String url="jdbc:mysql://localhost:3306/" +db;
-    public String user = "root";
-    public String pass = "12345";
+    public String user = "user";
+    public String pass = "0982820831Jose";
 
     public Conexion() {
     }
